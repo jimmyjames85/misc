b49b8ac1f8692ac4187612bd89a715610c6e614e
jimmyjames85 <jimmy.j@mes.com>
2014-10-07 22:11:17 -0500

    update

--- 309.2.11.s
+++ 309.2.11.s
@@ -1,32 +1,115 @@
 .data
-
-	.word 14
-	.word 15
-	.word 16
-	.word 17
-	.word 18
-	.word 19
-	.word 20
-	.word 21
-	.word 22
-	.word 23
-	.word 24
-	.word 25
-	.word 26
-	.word 27
-	.word 28
-	.word 29
-	.word 30
-	.word 4
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 3
+	.word 0
 	.word 1
 	.word 2
-	.word 333
+	.word 3
 	.word 4
 	.word 5
 	.word 6
-	.word 0
+	.word 7
 	.word 8
-	.word 91
+	.word 9
 	.word 10
 	.word 11
 	.word 12
@@ -48,179 +131,107 @@
 	.word 28
 	.word 29
 	.word 30
-	.word 4
-	.word 1
-	.word 2
-	.word 333
-	.word 4
-	.word 5
-	.word 6
-	.word 0
-	.word 8
+	.word 31
+	.word 32
+	.word 33
+	.word 34
+	.word 35
+	.word 36
+	.word 37
+	.word 38
+	.word 39
+	.word 40
+	.word 41
+	.word 42
+	.word 43
+	.word 44
+	.word 45
+	.word 46
+	.word 47
+	.word 48
+	.word 49
+	.word 50
+	.word 51
+	.word 52
+	.word 53
+	.word 54
+	.word 55
+	.word 56
+	.word 57
+	.word 58
+	.word 59
+	.word 60
+	.word 61
+	.word 62
+	.word 63
+	.word 64
+	.word 65
+	.word 66
+	.word 67
+	.word 68
+	.word 69
+	.word 70
+	.word 71
+	.word 72
+	.word 73
+	.word 74
+	.word 75
+	.word 76
+	.word 77
+	.word 78
+	.word 79
+	.word 80
+	.word 81
+	.word 82
+	.word 83
+	.word 84
+	.word 85
+	.word 86
+	.word 87
+	.word 88
+	.word 89
+	.word 90
 	.word 91
-	.word 10
-	.word 11
-	.word 12
-	.word 13
-	.word 14
-	.word 15
-	.word 16
-	.word 17
-	.word 18
-	.word 19
-	.word 20
-	.word 21
-	.word 22
-	.word 23
-	.word 24
-	.word 25
-	.word 26
-	.word 27
-	.word 28
-	.word 29
-	.word 30
-	.word 4
-	.word 1
-	.word 2
-	.word 333
-	.word 4
-	.word 5
-	.word 6
-	.word 0
-	.word 8
-	.word 91
-	.word 10
-	.word 11
-	.word 12
-	.word 13
-	.word 14
-	.word 15
-	.word 16
-	.word 17
-	.word 29
-	.word 30
-	.word 17
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-	.word 0
-
+	.word 92
+	.word 93
+	.word 94
+	.word 95
+	.word 96
+	.word 97
+	.word 98
+	.word 99
 	
 
-	.text
+.text
 .globl main
 	
 main:
+#	lui $a0, 0x1001
+#	addi $t0, $zero, 8
+#	add $t1, $a0, $t0
+#	lw $t1, 0($t1)
+#
+#	lui  $a1, 0x1001
+#	addi $a1, 400	 	#base addr of B is in a1
+#
+#	sw $t1, 0($a1)
+#	j end
 	lui  $a0, 0x1001 	#base addr of A is in a0
-	lui  $a1, 0x1001 	#base addr of B is in a1
-	addi $a1, 4000
+	lui  $a1, 0x1001
+	addi $a1, 400	 	#base addr of B is in a1
 	addi $s0, $zero, 7	# c=7
-loop:
 	addi $t0, $zero, 0	# t0 = 0
+loop:	
 	slti $t1, $t0, 404
 	beq  $t1, $zero, end
 	add  $t1, $a0, $t0
-	add  $t2, $a1, $s0
+	add  $t2, $a1, $t0
 	lw   $t2, 0($t2)
 	add  $t2, $t2, $s0
+	
 	sw   $t2, 0($t1)
 	addi $t0, $t0, 4
+	j loop
 end:	
 done: 
 	jr $ra
